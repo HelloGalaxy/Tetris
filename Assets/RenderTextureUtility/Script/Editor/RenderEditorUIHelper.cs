dd10a80e6505b088736d7496d713749a36a302f5
雨天 <77202714@qq.com>
2017-04-04 15:57:07 +0800

    render texture对齐用的小工具

---
+++ Assets/RenderTextureUtility/Script/Editor/RenderEditorUIHelper.cs
@@ -0,0 +1,31 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public static class RenderEditorUIHelper
+{
+    private static Camera GetRenderCamera()
+    {
+        var cameras = Camera.allCameras;
+        for (var i = 0; i < cameras.Length; i++)
+        {
+            var targetCam = cameras[i];
+            if (targetCam.targetTexture != null)
+            {
+                return targetCam;
+            }
+        }
+        return Camera.main;
+    }
+
+    [MenuItem("GameObject/居中对齐渲染的相机", false)]
+    public static void SetInTheCenterOfCamera()
+    {
+        var cam = GetRenderCamera();
+        var selectedObject = Selection.activeGameObject;
+        if (selectedObject != null && cam != null)
+        {
+            var pos = new Vector3(cam.transform.position.x, cam.transform.position.y, cam.transform.position.z + 10);
+            selectedObject.gameObject.transform.position = pos;
+        }
+    }
+}
---
+++ Assets/RenderTextureUtility/Script/MainSceneUI.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MainSceneUI : MonoBehaviour
+{
+    public Text content;
+    public RawImage rawImage;
+
+    private void Start()
+    {
+        content.text = string.Format("{0}: {1}x{2}", rawImage.texture.name, rawImage.texture.width, rawImage.texture.height);
+    }
+}
